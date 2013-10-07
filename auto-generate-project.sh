c50a79c627c41727c6a98ae9dd328e6b7c2643bb
Ricky <Ricky@sasvm>
2013-10-07 13:38:43 -0400

    Remove option that generate project file in source folder

--- auto-generate-project.sh
+++ auto-generate-project.sh
@@ -1,3 +1,4 @@
 mkdir nsight-build
 cd nsight-build
+#cmake -G "Eclipse CDT4 - Unix Makefiles" -D CMAKE_BUILD_TYPE=Debug -D CMAKE_ECLIPSE_GENERATE_SOURCE_PROJECT=TRUE ../src/
 cmake -G "Eclipse CDT4 - Unix Makefiles" -D CMAKE_BUILD_TYPE=Debug ../src/
