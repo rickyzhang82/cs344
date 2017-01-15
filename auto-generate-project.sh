1e2cb113101ad3c5d60196b058687f0e469ea5b9
Ricky Zhang <rickyzhang@gmail.com>
2017-01-15 17:30:15 -0500

    Fix bin folder
    Signed-off-by: Ricky Zhang <rickyzhang@gmail.com>

--- auto-generate-project.sh
+++ auto-generate-project.sh
@@ -1,4 +1,4 @@
-mkdir nsight-build
-cd nsight-build
+mkdir -p cs344-project/bin
+cd cs344-project
 #cmake -G "Eclipse CDT4 - Unix Makefiles" -D CMAKE_BUILD_TYPE=Debug -D CMAKE_ECLIPSE_GENERATE_SOURCE_PROJECT=TRUE ../src/
 cmake -G "Eclipse CDT4 - Unix Makefiles" -D CMAKE_BUILD_TYPE=Debug ../src/
